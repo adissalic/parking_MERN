239133303f921c8efa2e9f8a1f5bd9554ac8f631
adissalic <49042003+adissalic@users.noreply.github.com>
2023-08-10 23:48:54 +0200

    test conn

--- parking_backend/app.js
+++ parking_backend/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 });
 mongoose
   .connect(
-    `mongodb+srv://${MONGODB_USER}@cluster0.9fcw6ew.mongodb.net/parking?retryWrites=true&w=majority`
+    `mongodb+srv://todouser:Todo123@cluster0.9fcw6ew.mongodb.net/parking?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5500);
